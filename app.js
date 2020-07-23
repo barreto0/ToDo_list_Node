4555ec883b372faace2466caac88611f7909030d
barreto <antonio909790@yahoo.com.br>
2020-07-22 21:40:03 -0300

    mongoDB implementation

--- app.js
+++ app.js
@@ -9,44 +9,156 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
+const _ = require("lodash");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public")); //para poder carregar os aquivos estaticos do site, i.e css, plain javascript
 app.set("view engine","ejs");
 
-let newListItems = ["Cluster with tacometer for the clio","Coilover suspension","Refurbish the 14'"];
-let workListItems = [];
+mongoose.connect("mongodb://localhost:27017/todoListDB", {useNewUrlParser:true, useUnifiedTopology:true})
+.then(() => console.log('DB Connected!'))
+.catch(err => {
+console.log("DB Connection Error: "+err.message);
+});//especifica a porta por onde acessaremos o nosso banco de dados
+// se o banco de dados especificado (carsDB) não existe ele cria um novo com esse nome
+
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name field missing!"]
+    }
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+    name: "Welcome to your todo list!"
+});
+
+const item2 = new Item({
+    name: "Hit de + to add a new item to your list"
+});
+
+const item3 = new Item({
+    name: "<-- Click the checkbox to cross-out an item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name field missing!"]
+    },
+    items: [itemSchema]
+});
+
+const List = mongoose.model("List", listSchema);
+
+let dia = date.getDay();
 
 app.get("/", function(req, res){
-   
+
     let dia = date.getDay();
 
-    res.render("list",{ //o valor chave é o nome da variável a ser modificada no .ejs
-        title: dia,  
-        newListItems: newListItems
+    Item.find(function(err, items){
+        if(items.length === 0){
+            Item.insertMany(defaultItems, function(err){
+                if(err){
+                    console.log(err.message);
+                }else{
+                    console.log("Default data inserted, no errors!");
+                }
+            });
+            res.redirect("/");
+        }else{
+            if(err){
+                console.log(err.message);
+            }else{
+                res.render("list",{ //o valor chave é o nome da variável a ser modificada no .ejs
+                    title: dia,  
+                    newListItems: items
+                });
+            }
+        }
     });
-
 });
 
 app.post("/",function(req,res){
-    let newListItem = req.body.newItem;
-   
-    if(req.body.botaoLista === "Trabalho"){
-        workListItems.push(newListItem);
-        res.redirect("/work");
-    }else{
-        newListItems.push(newListItem);
+
+    const itemName = req.body.newItem;
+    const listName = req.body.botaoLista; //name button submit
+
+    const item = new Item({
+        name: itemName
+    });
+
+    if(listName === dia){
+        item.save();
         res.redirect("/");
+    }else{
+        List.findOne({name: listName}, function(err,result){
+            if(err){
+                console.log(err.message);
+            }else{
+            result.items.push(item);
+            result.save();
+            res.redirect("/"+listName);
+            }
+        });
     }
-    
 });
 
-app.get("/work",function(req,res){
-    res.render("list", {
-        title: "Trabalho",
-        newListItems: workListItems
+app.post("/delete", function(req,res){
+    const checkboxItemId = req.body.checkbox;
+    const listName = req.body.listName;
+
+    if(listName === dia){
+        Item.findByIdAndDelete(checkboxItemId, function(err){
+            if(err){
+                console.log(err.message);
+            }else{
+                console.log("Item removed with no errors!");
+            }
+        });
+        res.redirect("/");
+    }else{
+        List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkboxItemId}}}, function(err, result){
+            if(err){
+                console.log(err.message);
+            }else{
+                res.redirect("/"+listName);
+            }
+        });//The $pull operator removes from an existing array 
+           //all instances of a value or values that match a specified condition.
+    }
+});
+
+app.get("/:customListName", function(req,res){
+
+    const listName = _.capitalize(req.params.customListName); //primeira letra sempre maiúscula, assim qualquer url digita resultará na mesma lista
+
+    List.findOne({name: listName}, function(err, result){
+        if(err){
+            console.log(err.message);
+        }else{
+            if(!result){
+                //cria uma nova lista
+                const list = new List({
+                    name: listName,
+                    items: defaultItems
+                });
+                list.save();
+                res.redirect("/"+listName);
+            }else{
+                //mostra uma lista já existente
+                res.render("list", {title: result.name, newListItems: result.items});
+            }
+        }
     });
 });
 
--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
 # Todo List
 
-🇧🇷 PT-BR: Simples Todo list feita com o intuito de estudar mais sobre o ambiente Node.js e alguns de seus pacotes mais populares: Express, EJS e Mongoose.
+🇧🇷 PT-BR: Simples Todo list feita com o intuito de estudar mais sobre o ambiente Node.js e alguns de seus pacotes mais populares: Express, EJS e Mongoose. É possível criar diversas listas diferentes com persistência de dados (tarefas) digitando um nome qualquer na própria url i.e *endereço.com/nome_da_lista*. A lista na rota root tem o título de acordo com o dia da semana em inglês, a função que trata da data vem de um módulo local *date.js*. Toda a lógica reside no servidor (Express+Mongoose), e o frontend usa templates EJS e uma interface genérica retirada da internet.
 
-🇺🇸 EN-US: Simple Todo list made in order to study more about the Node.js environment and some of its most popular packages: Express, EJS and Mongoose.
+🇺🇸 EN-US: Simple Todo list made in order to study more about the Node.js environment and some of its most popular packages: Express, EJS and Mongoose. You can create several different lists with data persistence (tasks) by typing any name in the url itself i.e. *address.com/list_name*. The list on the root route is titled according to the day of the week in English, the function that handles the date comes from a local *date.js* module. All the logic resides on the server (Express+Mongoose), and the frontend uses EJS templates and a generic interface taken from the internet.
+
 
 ![screenshot1](screenshots/1.png)
 ![screenshot2](screenshots/2.png)
-![screenshot3](screenshots/3.png)
